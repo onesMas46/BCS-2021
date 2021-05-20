2de11a4a089df0cf7897ff2265e2d9666de60f03
onesMas46 <ones.ran46@outlook.com>
2021-05-20 11:40:22 +0300

    exercise5 edit

--- src/chapter8/exercise5.py
+++ src/chapter8/exercise5.py
@@ -1,14 +1,15 @@
 fname = "mbox_short.txt"
+file = open(fname)
+index = 0
 count = 0
-try:
-    fhand = open(fname)
-except:
-    print("File cannot be opened:", fname)
-    quit()
-for line in fhand:
-    if not line.startswith("From: "):
-        continue
+
+for line in file:
     line = line.rstrip()
-    print(line)
+    if not line.startswith('From'):
+        continue
     count += 1
-print("There were", count, "lines in the file with From as the first word")
+    index = line.find('From') + 1
+    word = line.split()
+    print(word[index])
+
+print("There were",count,"lines in the file with From as the first word")
